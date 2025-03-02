c4df2f38e6da9ccf115aa77d6e868aef64942205
eswr <easwarendra.ece@gmail.com>
2025-03-02 17:11:45 +0530

    add ubuntu/mac common config

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nix-darwin,
+    home-manager,
+  }: let
+    makeHomeManagerConfiguration = {
+      system,
+      username,
+      homeDirectory ? "/home/${username}",
+    }: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./modules/home.nix
+          {
+            home = {
+              inherit homeDirectory username;
+              stateVersion = "23.11";
+            };
+          }
+        ];
+      };
+  in {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    homeConfigurations.eswr = makeHomeManagerConfiguration {
+      system = "x86_64-linux";
+      username = "eswr";
+    };
+    homeConfigurations.eswr-ci = makeHomeManagerConfiguration {
+      system = "x86_64-linux";
+      username = "runner";
+      homeDirectory = "/home/runner";
+    };
+    darwinConfigurations.pearwin-laptop = nix-darwin.lib.darwinSystem {
+      system = "x86_64-linux";
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./modules/macos.nix
+      ];
+    };
+  };
+}
